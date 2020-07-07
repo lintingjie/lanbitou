5fa6ebca4f663aaca21ddae38186b4ba0c71603d
lintingjie <lintingjie@hikvision.com>
2020-07-07 16:17:28 +0800

    用例

--- annotation/exampleVO.java
+++ annotation/exampleVO.java
@@ -12,21 +12,11 @@ import lombok.Data;
  * @date 2020/5/6 14:16
  */
 @Data
-public class AppActivityVO {
+public class exampleVO {
 
     @ApiModelProperty(value = "活动id", notes = "活动id", dataType = "String")
     @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
-
-    @ApiModelProperty(value = "课程id", notes = "课程id", dataType = "String")
-    @JsonSerialize(using = ToStringSerializer.class)
-    private Long courseId;
-
-    @ApiModelProperty(value = "banner图片", notes = "banner图片", dataType = "String")
-    private String bannerUrl;
-
-    @ApiModelProperty(value = "支付状态，1已购买，2未购买", notes = "支付状态，1已购买，2未购买", dataType = "Integer")
-    private Integer payStatus;
 
     @ApiModelProperty(value = "课程价格", notes = "课程价格", dataType = "Double")
     @JsonSerialize(using = CustomDoubleSerialize.class)
